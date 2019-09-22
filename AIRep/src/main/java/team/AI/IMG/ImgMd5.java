4d95b56805fb15cd58131869a256584ac3ef7144
lmk <1583214829@qq.com>
2019-09-22 23:15:08 +0800

    lmk

--- AIRep/src/main/java/team/AI/IMG/ImgMd5.java
+++ AIRep/src/main/java/team/AI/IMG/ImgMd5.java
@@ -1,27 +1,25 @@
 package team.AI.IMG;
 
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.IOUtils;
-
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+
 public class ImgMd5 {
 
-public static void main(String[] args) throws IOException {
-        // TODO Auto-generated method stub
-        String path="";
-        String path2="";
-
-        FileInputStream fis= new FileInputStream(path);
-        String md5 = DigestUtils.md5Hex(IOUtils.toByteArray(fis));
-        IOUtils.closeQuietly(fis);
-        System.out.println("p1_MD5:"+md5);
-
-        FileInputStream fis2= new FileInputStream(path2);
-        String md52 = DigestUtils.md5Hex(IOUtils.toByteArray(fis2));
-        IOUtils.closeQuietly(fis2);
-        System.out.println("p2_MD5:"+md52);
-
+    public String getMd5(File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            return DigestUtils.md5Hex(IOUtils.toByteArray(fileInputStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static void main(String[] args) throws Exception {
+        //System.out.println(getMd5(new File("/Users/apple/Desktop/img.zip")));
+    }
+
+
 }
--- HackerWeb/src/main/java/team/AI/servlet/SensitiveWordServlet.java
+++ HackerWeb/src/main/java/team/AI/servlet/SensitiveWordServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/SensitiveWord")
@@ -18,6 +19,20 @@ public class SensitiveWordServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
+        /*
+                计算任务数和正在进行的任务数
+         */
+        HttpSession session = req.getSession();
+        int sum=(int)session.getAttribute("sum");
+        int count =(int) session.getAttribute("count");
+        count++;
+        sum++;
+        session.setAttribute("count",count);
+        session.setAttribute("sum",sum);
+
+
+
+
 
         String choose = req.getParameter("choose");
         String url = req.getParameter("url");
