6517133c3c20aa3826dc276f44ae567f2c275fd6
wzy123hhh <319732708@qq.com>
2019-09-22 23:18:35 +0800

    new project

--- HackerWeb/src/main/java/team/AI/serviceIMP/SensitiveWordServiceIMP.java
+++ HackerWeb/src/main/java/team/AI/serviceIMP/SensitiveWordServiceIMP.java
@@ -12,16 +12,14 @@ import java.util.Map;
 public class SensitiveWordServiceIMP implements SenesitiveWordService {
 
     @Override
-    public int startCrawler(String url, int... type) {
+    public String startCrawler(String url, int... type) {
         ArrayList<UrlInfo> urlInfos = WebsiteProcessor.StartCrawler(url, type);
-        System.out.println(urlInfos);
-        getResult(urlInfos);
-        return 0;
+        System.out.println("==========="+urlInfos);
+        return  getResult(urlInfos);
     }
 
     @Override
     public String getResult(ArrayList<UrlInfo> infolist) {
-        System.out.println("sss");
         //未爬取到信息
         if (infolist.size()==0){
             Map result= new HashMap();
@@ -30,9 +28,8 @@ public class SensitiveWordServiceIMP implements SenesitiveWordService {
             System.out.println(JSONObject.toJSONString(result));
             return JSONObject.toJSONString(result);
         }else{
-            statistics(infolist);
+            return statistics(infolist);
         }
-        return null;
     }
 
     /**
@@ -42,6 +39,13 @@ public class SensitiveWordServiceIMP implements SenesitiveWordService {
      */
     @Override
     public String statistics(ArrayList<UrlInfo> infolist) {
+        int zero=0;
+        int one=0;
+        int two=0;
+        int three=0;
+        int four=0;
+        int five=0;
+        int total=0;
         /**
          * 0：暴恐敏感词
          * 1：反动敏感词
@@ -53,11 +57,23 @@ public class SensitiveWordServiceIMP implements SenesitiveWordService {
         for (UrlInfo info : infolist) {
             if (info.getHits()!="无敏感词"){
                 System.out.println(info.getHits());
-
+                if (info.getHits().contains("0")){
+                    zero++;
+                }if (info.getHits().contains("1")){
+                    one++;
+                }if (info.getHits().contains("2")){
+                    two++;
+                }if (info.getHits().contains("3")){
+                    three++;
+                }if (info.getHits().contains("4")){
+                    four++;
+                }if (info.getHits().contains("5")){
+                    five++;
+                }
             }
         }
-        return null;
-
+        total=zero+one+two+three+four+five;
+        return "["+zero+","+one+","+two+","+three+","+four+","+five+"]";
     }
 
 
--- HackerWeb/target/HackerWeb/assets/js/pages/demo.chartjs.js
+++ HackerWeb/target/HackerWeb/assets/js/pages/demo.chartjs.js
@@ -1,1 +1,83 @@
-!function(r){"use strict";var a=function(){this.$body=r("body"),this.charts=[]};a.prototype.respChart=function(a,t,e,o){var n=Chart.controllers.line.prototype.draw;Chart.controllers.line.prototype.draw=function(){n.apply(this,arguments);var r=this.chart.chart.ctx,a=r.stroke;r.stroke=function(){r.save(),r.shadowColor="rgba(0,0,0,0.01)",r.shadowBlur=20,r.shadowOffsetX=0,r.shadowOffsetY=5,a.apply(this,arguments),r.restore()}};var s=Chart.controllers.doughnut.prototype.draw;Chart.controllers.doughnut=Chart.controllers.doughnut.extend({draw:function(){s.apply(this,arguments);var r=this.chart.chart.ctx,a=r.fill;r.fill=function(){r.save(),r.shadowColor="rgba(0,0,0,0.03)",r.shadowBlur=4,r.shadowOffsetX=0,r.shadowOffsetY=3,a.apply(this,arguments),r.restore()}}});var l=Chart.controllers.bar.prototype.draw;Chart.controllers.bar=Chart.controllers.bar.extend({draw:function(){l.apply(this,arguments);var r=this.chart.chart.ctx,a=r.fill;r.fill=function(){r.save(),r.shadowColor="rgba(0,0,0,0.01)",r.shadowBlur=20,r.shadowOffsetX=4,r.shadowOffsetY=5,a.apply(this,arguments),r.restore()}}}),Chart.defaults.global.defaultFontColor="#8391a2",Chart.defaults.scale.gridLines.color="#8391a2";var i=a.get(0).getContext("2d"),d=r(a).parent();return function(){var n;switch(a.attr("width",r(d).width()),t){case"Line":n=new Chart(i,{type:"line",data:e,options:o});break;case"Doughnut":n=new Chart(i,{type:"doughnut",data:e,options:o});break;case"Pie":n=new Chart(i,{type:"pie",data:e,options:o});break;case"Bar":n=new Chart(i,{type:"bar",data:e,options:o});break;case"Radar":n=new Chart(i,{type:"radar",data:e,options:o});break;case"PolarArea":n=new Chart(i,{data:e,type:"polarArea",options:o})}return n}()},a.prototype.initCharts=function(){var a=[];if(r("#line-chart-example").length>0){a.push(this.respChart(r("#line-chart-example"),"Line",{labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],datasets:[{label:"Current Week",backgroundColor:"rgba(114, 124, 245, 0.3)",borderColor:"#727cf5",data:[32,42,42,62,52,75,62]},{label:"Previous Week",fill:!0,backgroundColor:"transparent",borderColor:"#0acf97",borderDash:[5,5],data:[42,58,66,93,82,105,92]}]},{maintainAspectRatio:!1,legend:{display:!1},tooltips:{intersect:!1},hover:{intersect:!0},plugins:{filler:{propagate:!1}},scales:{xAxes:[{reverse:!0,gridLines:{color:"rgba(0,0,0,0.05)"}}],yAxes:[{ticks:{stepSize:20},display:!0,borderDash:[5,5],gridLines:{color:"rgba(0,0,0,0)",fontColor:"#fff"}}]}}))}if(r("#bar-chart-example").length>0){var t=document.getElementById("bar-chart-example").getContext("2d").createLinearGradient(0,500,0,150);t.addColorStop(0,"#fa5c7c"),t.addColorStop(1,"#727cf5");var e={labels:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],datasets:[{label:"Sales Analytics",backgroundColor:t,borderColor:t,hoverBackgroundColor:t,hoverBorderColor:t,data:[65,59,80,81,56,89,40,32,65,59,80,81]},{label:"Dollar Rate",backgroundColor:"#e3eaef",borderColor:"#e3eaef",hoverBackgroundColor:"#e3eaef",hoverBorderColor:"#e3eaef",data:[89,40,32,65,59,80,81,56,89,40,65,59]}]};a.push(this.respChart(r("#bar-chart-example"),"Bar",e,{maintainAspectRatio:!1,legend:{display:!1},scales:{yAxes:[{gridLines:{display:!1,color:"rgba(0,0,0,0.05)"},stacked:!1,ticks:{stepSize:20}}],xAxes:[{barPercentage:.7,categoryPercentage:.5,stacked:!1,gridLines:{color:"rgba(0,0,0,0.01)"}}]}}))}if(r("#donut-chart-example").length>0){a.push(this.respChart(r("#donut-chart-example"),"Doughnut",{labels:["Direct","Affilliate","Sponsored","E-mail"],datasets:[{data:[300,135,48,154],backgroundColor:["#727cf5","#fa5c7c","#0acf97","#ebeff2"],borderColor:"transparent",borderWidth:"3"}]},{maintainAspectRatio:!1,cutoutPercentage:60,legend:{display:!1}}))}if(r("#radar-chart-example").length>0){a.push(this.respChart(r("#radar-chart-example"),"Radar",{labels:["Eating","Drinking","Sleeping","Designing","Coding","Cycling","Running"],datasets:[{label:"Desktops",backgroundColor:"rgba(57,175,209,0.2)",borderColor:"#39afd1",pointBackgroundColor:"#39afd1",pointBorderColor:"#fff",pointHoverBackgroundColor:"#fff",pointHoverBorderColor:"#39afd1",data:[65,59,90,81,56,55,40]},{label:"Tablets",backgroundColor:"rgba(161, 127, 224,0.2)",borderColor:"#a17fe0",pointBackgroundColor:"#a17fe0",pointBorderColor:"#fff",pointHoverBackgroundColor:"#fff",pointHoverBorderColor:"#a17fe0",data:[28,48,40,19,96,27,100]}]},{maintainAspectRatio:!1}))}return a},a.prototype.init=function(){var a=this;Chart.defaults.global.defaultFontFamily='-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',a.charts=this.initCharts(),r(window).on("resize",function(t){r.each(a.charts,function(r,a){try{a.destroy()}catch(r){}}),a.charts=a.initCharts()})},r.ChartJs=new a,r.ChartJs.Constructor=a}(window.jQuery),function(r){"use strict";r.ChartJs.init()}(window.jQuery);
+var ctx=document.getElementById("radar-chart-example");
+var ctx0=document.getElementById("donut-chart-example");
+
+var circle =new Chart(ctx0, {
+    type: 'doughnut'// or 'pie'
+    ,data: {
+        datasets: [{
+            data: [300, 135, 48, 154,50,600],
+            backgroundColor: ["#727cf5", "#fa5c7c", "#0acf97", "#ebeff2","#00EE00","#20B2AA"],
+            borderColor: "transparent",
+            borderWidth: "3"
+        }],
+        labels: ["暴恐", "反动", "色情", "其他","民生","自定义"]
+    },
+});
+
+var leida=new Chart(ctx, {
+    type: 'radar'
+    ,data:{
+        labels: ["暴恐敏感词", "反动敏感词", "色情敏感词", "其他敏感词","民生敏感词","自定义敏感词"],
+        datasets: [{
+            label: "敏感词",
+            backgroundColor: "rgba(57,175,209,0.2)",
+            borderColor: "#39afd1",
+            pointBackgroundColor: "#39afd1",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "#39afd1",
+            data: [200, 59, 90, 81, 56, 55]
+        }]
+    },
+    options: {
+        // 需要特别注意的是: 这里的scale没有s
+        scale: {
+            ticks: {
+                min: 0
+            }
+        }
+    }
+});
+
+
+function submitCheck() {
+
+    //禁用查询按钮
+    $("#submit-minganci").attr("disable","true")
+
+    //不显示图表
+    document.getElementById("result-chart").style.display="none";
+
+    //获取查询内容
+    var choose= $('#wordschoose').val().toString()
+    var content=$('#summernote-editmode').summernote('code')
+    var url = $('#curl').val()
+
+    $.ajax({
+        type: "post",
+        url: "SensitiveWord",
+        data: {"choose":choose,"content":content,"url":url},
+        dataType: "json",
+        success: function (res) {
+            if (res.result=="fail") {
+                $.NotificationApp.send("查询失败", "输入的url不正确，或该站点禁制爬虫访问。", "top-center","rgba(0,0,0,0.2)","fail")
+            }else{
+                var data=res.toString().split(",")
+                var nums =new Array(6);
+               for (var i=0;i<data.length;i++){
+                   nums[i]=parseInt(data[i]);
+               }
+                console.log(nums.type)
+                document.getElementById("result-chart").style.display="";
+                circle.data.datasets[0].data=nums;
+                leida.data.datasets[0].data=nums;
+                circle.update();
+                leida.update();
+            }
+            $("#submit-minganci").attr("disable","false")
+        }
+    });
+}
+
+
+
